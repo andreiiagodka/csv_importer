d41f9bf2cb3ed82b709a3c1847475c0c6057f51a
andreiiagodka <andrei.iagodka@gmail.com>
2021-11-20 13:51:46 +0200

    Rethink logic of retrieving model

--- app/concepts/supplier/operation/save.rb
+++ app/concepts/supplier/operation/save.rb
@@ -1,8 +1,12 @@
 module Supplier::Operation
   class Save < Trailblazer::Operation
-    step Model(Supplier, :new)
+    step :model
     step Contract::Build(constant: Supplier::Contract::Save)
     step Contract::Validate(key: :supplier)
     step Contract::Persist()
+
+    def model(ctx, params:, **)
+      ctx[:model] = Supplier.find_by(code: params[:supplier][:code]) || Supplier.new
+    end
   end
 end
