cc00544a8b711baee4601aef3fa987ab804390e0
andreiiagodka <andrei.iagodka@gmail.com>
2021-11-30 18:41:48 +0200

    Add test cases to worker unit tests

--- spec/workers/csv/products_import_worker_spec.rb
+++ spec/workers/csv/products_import_worker_spec.rb
@@ -7,7 +7,19 @@ RSpec.describe Csv::ProductsImportWorker, type: :worker do
 
   before { create :supplier, :import }
 
-  it 'asserts Product count' do
-    expect { worker }.to change(Product, :count).from(0).to(1)
+  context 'when sidekiq testing fake' do
+    before { Sidekiq::Testing.fake! }
+
+    it 'asserts jobs count' do
+      expect { worker }.to change(described_class.jobs, :size).by(1)
+    end
+  end
+
+  context 'when sidekiq testing inline' do
+    before { Sidekiq::Testing.inline! }
+
+    it 'asserts Product count' do
+      expect { worker }.to change(Product, :count).from(0).to(1)
+    end
   end
 end
