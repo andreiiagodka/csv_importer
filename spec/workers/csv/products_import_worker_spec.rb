8f9e71664f4c79a474c994026648de6ef705b4dc
andreiiagodka <andrei.iagodka@gmail.com>
2021-12-02 19:53:19 +0200

    Fix worker rspecs

--- spec/workers/csv/products_import_worker_spec.rb
+++ spec/workers/csv/products_import_worker_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Csv::ProductsImportWorker, type: :worker do
-  subject(:worker) { described_class.perform_async(file) }
+  subject(:worker) { described_class.perform_async(rows) }
 
-  let(:file) { Rails.root.join('spec', 'fixtures', 'products.csv') }
+  let(:rows) do 
+    file = Rails.root.join('spec', 'fixtures', 'products.csv')
+    parser = Csv::ProductParser.new(file)
+    parser.call
+
+    parser.rows
+  end
 
   before { create :supplier, :import }
 
--- spec/workers/csv/suppliers_import_worker_spec.rb
+++ spec/workers/csv/suppliers_import_worker_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Csv::SuppliersImportWorker, type: :worker do
-  subject(:worker) { described_class.perform_async(file) }
+  subject(:worker) { described_class.perform_async(rows) }
 
-  let(:file) { Rails.root.join('spec', 'fixtures', 'suppliers.csv') }
+  let(:rows) do 
+    file = Rails.root.join('spec', 'fixtures', 'suppliers.csv')
+    parser = Csv::SupplierParser.new(file)
+    parser.call
+
+    parser.rows
+  end
 
   it 'asserts Supplier count' do
     expect { worker }.to change(Supplier, :count).from(0).to(1)
